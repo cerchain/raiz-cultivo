3cd0233c1c999b4feae270487734b5d155fb29b7
cerchain <117107755+cerchain@users.noreply.github.com>
2026-07-06 10:55:29 -0300

    Add files via upload

---
+++ src/supabaseClient.js
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://ymtkfdjshhlfezghteja.supabase.co'
+const supabaseKey = 'sb_publishable_O55A96TRQHE_Tbjj-4WekQ_erW9ANdv'
+
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  global: {
+    fetch: (url, options = {}) => fetch(url, { ...options, cache: 'no-store' })
+  }
+})
